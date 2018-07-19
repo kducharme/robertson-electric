cd57dca1853266c01a85bd070c92ba0febb6e184
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-07-18 20:46:23 -0500

    hero added

---
+++ src/components/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hero">
+    <Nav />
+    <div class="hero__cta">
+      <h1>{{ msg }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import Nav from './Nav.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Nav
+  },
+  data () {
+    return {
+      msg: 'High quality electrical a vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum.'
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+  .hero {
+    background-image: url('../img/hero3.jpg');
+    background-size: 100vw;
+    width: 100vw;
+    height: 80vh;
+    .hero__cta {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: calc(80vh);
+      top: 0;
+      margin-top: -60px;
+      background: rgba(0,0,0,.3);
+      padding: 0 60px;
+      text-align: center;
+    h1 {
+      color: white;
+    }
+    }
+    
+  }
+
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav>
+    <p>Robinson Electric</p>
+    <ul>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data () {
+    return {
+      msg: "Robinson Electric"
+    };
+  }
+};
+</script>
+
+<style lang='scss'>
+nav {
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 40px;
+  ul {
+    list-style: none;
+  }
+  p {
+    color: white;
+    font-size: 16px;
+    font-weight: 600;
+  }
+  ul {
+      display: flex;
+      li {
+          margin-right: 16px;
+          color: white;
+          font-size: 14px;
+      }
+  }
+}
+</style>
